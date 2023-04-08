41c904d898f363dd95383377ea2aba6937f894a7
Paulo Marques <paulo.marques@incognito.com>
2023-04-08 15:37:39 -0700

    custom user model

--- vinumxp/settings.py
+++ vinumxp/settings.py
@@ -26,6 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+
+AUTH_USER_MODEL = 'users.User'
+
+
 ALLOWED_HOSTS = []
 
 
